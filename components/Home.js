43e967e38b8881f46b0710cf479b77efc08a8c2a
Matteo <pro.matteo.piselli@gmail.com>
2025-05-02 10:56:57 +0200

    first commit frontend

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,18 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.main}>
+      <img
+        src="/background.png"
+        alt="background-kackatweet"
+        className={styles.bg}
+      />
+      <img src="/tweeter-logo.png" alt="logo twitter" className={styles.logo} />
+      <h1>See what's happening</h1>
+      <h2>Join Hackatweet today.</h2>
+      <button>Signup</button>
+      <button>Signin</button>
     </div>
   );
 }
